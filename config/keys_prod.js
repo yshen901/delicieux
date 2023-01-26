88963735bfedbeff8fe52f700f8c132f5e186ac0
Yuci Shen <shen.yuci1@gmail.com>
2023-01-26 01:05:55 -0800

    Update apiKey with process env.

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: process.env.mongoURI,
     secretOrKey: process.env.secretOrKey,
-    apiKey: "fe798df719mshf00aae41c9b30d2p11343ajsn966e13b425e1"
+    apiKey: process.env.apiKey
 }
--- frontend/src/util/key.js
+++ frontend/src/util/key.js
@@ -1,3 +1,3 @@
 export default {
-  apiKey: process.env.apiKey,
+  apiKey: process.env.apiKey
 }
